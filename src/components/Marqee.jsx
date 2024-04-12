0ca2f4b015e3a394c95da2e5a688719151bc649b
KesharwaniShivam <shivamkesharwani6767@gmail.com>
2024-04-13 00:33:12 +0530

    marqee motion changed

--- src/components/Marqee.jsx
+++ src/components/Marqee.jsx
@@ -7,7 +7,7 @@ function Marqee() {
     <>
     <div className='py-14 w-full rounded-tl-2xl rounded-tr-2xl bg-[#004D43]'>
     <div className='text border-t-2 border-b-2 border-zinc-400 flex  overflow-hidden gap-6 whitespace-nowrap'>
-       <motion.h1 initial={{  x: "30%" }} animate={{  x: "-100%" }}  transition={{ease:"linear", repeat:Infinity, duration: 8 }} 
+       <motion.h1 initial={{  x: 0 }} animate={{  x: "-100%" }}  transition={{ease:"linear", repeat:Infinity, duration: 8 }} 
        className='uppercase text-[17vw] tracking-tighter leading-none font-bold text-zinc-100 -mb-3 -mt-11'> we are ochi</motion.h1>
        
        <motion.h1 initial={{ x: 0 }} animate={{x: "-100%" }}  transition={{ease:"linear", repeat:Infinity, duration: 8 }} 
